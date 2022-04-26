d6c6555e899104808cc6f52a11cec002d18fcb90
Annas <61279556+annasajkh@users.noreply.github.com>
2022-04-26 21:53:49 +0700

    Create gpt.py

---
+++ gpt.py
@@ -0,0 +1,177 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from tqdm import tqdm
+from torch.optim import AdamW
+from IPython.display import clear_output
+
+
+class SwiGLU(nn.Module):
+    def forward(self, x):
+        x, gate = x.chunk(2, dim=-1)
+        return F.silu(gate) * x
+
+
+
+class TransformerBlock(nn.Module): 
+    def __init__(
+        self,
+        d_model,
+        n_heads,
+        mlp_scale=4,
+        attn_mask=None,
+        attn_drop=0,
+        resid_pdrop=0
+    ):
+        super().__init__()
+        self.attn_mask = attn_mask
+        self.attn = nn.MultiheadAttention(d_model, n_heads, dropout=attn_drop)
+        
+        self.attn_layer_norm = nn.LayerNorm(d_model)
+        self.mlp_layer_norm = nn.LayerNorm(d_model)
+        
+        self.mlp = nn.Sequential(
+            nn.Linear(d_model, d_model * mlp_scale),
+            SwiGLU(),
+            nn.Linear(d_model * mlp_scale, d_model),
+            nn.Dropout(resid_pdrop)
+        )
+    
+    def attention(self, x): 
+        if self.attn_mask is not None:
+            self.attn_mask = self.attn_mask.to(dtype=x.dtype, device=x.device)
+        
+        return self.attn(x, x, x, attn_mask=self.attn_mask, need_weights=False)[0]
+    
+    def forward(self, x):
+        x = x + self.attention(self.attn_layer_norm(x)) 
+        x = x + self.mlp(self.mlp_layer_norm(x))
+        return x
+
+
+
+class Transformer(nn.Module): 
+    def __init__(
+        self,
+        n_context,
+        n_embed,
+        n_heads,
+        n_layers, 
+        mlp_scale=4,
+        attn_mask=None,
+        attn_drop=0,
+        resid_pdrop=0,
+        embed_pdrop=0
+    ):
+        super().__init__()
+        
+        self.drop = nn.Dropout(embed_pdrop) 
+        self.pos_embed = nn.Parameter(torch.randn(n_context, n_embed)) 
+        self.layers = nn.Sequential(*[TransformerBlock(n_embed, n_heads, attn_mask=attn_mask, mlp_scale=mlp_scale, attn_drop=attn_drop, resid_pdrop=resid_pdrop) for _ in range(n_layers)]) 
+        self.ln_pre = nn.LayerNorm(n_embed)
+
+    #the input is an embbeding with this shape (batch, n_context, n_embed) 
+    def forward(self, x):
+        x = self.drop(x + self.pos_embed)
+        x = self.ln_pre(x) 
+
+        x = x.permute(1, 0, 2)
+        x = self.layers(x) 
+        x = x.permute(1, 0, 2)
+
+        return x
+
+
+class GPT(nn.Module):
+    def __init__(
+        self,
+        n_heads,
+        n_layers,
+        n_embed,
+        block_size,
+        n_vocab,
+        ignore_token,
+        mlp_scale=4,
+        attn_drop=0,
+        resid_pdrop=0,
+        embed_pdrop=0
+    ):
+        super().__init__()
+        
+        self.embed = nn.Embedding(n_vocab, n_embed)
+        
+        self.transformer = Transformer(n_context=block_size,
+                                       n_embed=n_embed,
+                                       n_heads=n_heads,
+                                       n_layers=n_layers,
+                                       mlp_scale=mlp_scale,
+                                       attn_mask=torch.triu(torch.full((block_size, block_size), float("-inf")), diagonal=1),
+                                       attn_drop=attn_drop,
+                                       resid_pdrop=resid_pdrop,
+                                       embed_pdrop=embed_pdrop)
+        
+        self.ln_post = nn.LayerNorm(n_embed)
+        self.out_head = nn.Linear(n_embed, n_vocab, bias=False)
+        self.ignore_token = ignore_token
+        self.block_size = block_size
+        
+        self.apply(self._init_weights)
+    
+    def _init_weights(self, module):
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if isinstance(module, nn.Linear) and module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.LayerNorm):
+            torch.nn.init.zeros_(module.bias)
+            torch.nn.init.ones_(module.weight)
+        elif isinstance(module, GPT):
+            torch.nn.init.normal_(module.transformer.pos_embed, mean=0.0, std=0.02)
+    
+    def forward(self, x, y=None):
+        x = self.embed(x)
+        x = self.transformer(x)
+        x = self.ln_post(x)
+        
+        logits = self.out_head(x)
+        
+        loss = None
+        
+        if y is not None:
+            loss = F.cross_entropy(logits.transpose(1, 2), y, ignore_index=self.ignore_token)
+        
+        return logits, loss
+    
+    @torch.no_grad()
+    def sample(self, x, temperature=1.0, top_k=40, max_length=100, batch_size=1):
+        x = torch.cat([torch.tensor([self.ignore_token, ]).to(device), x])
+        
+        batch = torch.stack([torch.cat([x, torch.full((self.block_size - len(x),), self.ignore_token).to(device)]) for _ in range(0, batch_size)]).to(x.device)
+        
+        
+        length = self.block_size if len(x) - 1 + max_length > self.block_size else len(x) - 1 + max_length
+        
+        for i in tqdm(range(len(x) - 1, length)): 
+            logits = self(batch)[0][:, i]
+            
+            logits = F.softmax(logits / temperature, dim=1)
+            logits = torch.topk(logits, top_k)
+
+            out = logits[1][:, torch.multinomial(logits[0], num_samples=1)]
+
+            if torch.all(out) and out[0] == self.ignore_token:
+                break
+            
+            batch[:, i + 1] = out
+
+        return batch
+
+
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+
+model = GPT(n_heads=6, n_layers=6, n_embed=768, block_size=1024, n_vocab=50257, ignore_token=50,256)
+# model.load_state_dict(torch.load("transformer.pkl"))
+model.to(device)
+optimizer = AdamW(model.parameters(), lr=3e-4)
